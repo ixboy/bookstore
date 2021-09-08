87c2b2b1f6595776fde0036aa14949222c8b6b62
ixboy <ismaelixboy@gmail.com>
2021-09-08 20:16:47 +0100

    generate categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/update'
+  get 'categories/destroy'
+  get 'categories/edit'
   get 'books/index'
   get 'books/show'
   get 'books/new'
