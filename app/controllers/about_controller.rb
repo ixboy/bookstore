474537f727ec775a91c35634191d99c56dee4034
ixboy <ismaelixboy@gmail.com>
2021-09-09 01:01:41 +0100

    edit routes

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,5 @@
 class AboutController < ApplicationController
   def index
+    @about_us = 'Welcome to Our page'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,9 @@
 Rails.application.routes.draw do
-  get 'publishers/index'
-  get 'publishers/show'
-  get 'publishers/new'
-  get 'publishers/create'
-  get 'publishers/update'
-  get 'publishers/destroy'
-  get 'publishers/edit'
-  get 'authors/index'
-  get 'authors/show'
-  get 'authors/new'
-  get 'authors/create'
-  get 'authors/update'
-  get 'authors/destroy'
-  get 'authors/edit'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'categories/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/update'
-  get 'books/destroy'
-  get 'books/edit'
-  get 'about/index'
+  root 'books#index'  
+  resources :authors
+  resources :categories
+  resources :books
+  resources :publishers # , only: %i[new create destroy]
+  resources :about, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
